67253bbaf608948bac914bd4da18123103005204
Juan Robledo <juannrob@gmail.com>
2022-07-11 20:15:02 -0300

    API latests & conversor functions created, latest section implemented

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,29 @@
+
+import React, { useEffect, useState } from 'react'
+import { latestRates } from '../services/API';
 import Title from '../components/Title';
 import './App.css';
+import Rates from '../components/Rates';
+
+//Acá están aplicados los componentes principales y los estados para pasarles
 
 const App = () => {
+
+  const [base, setBase] = useState('USD'); //define la base para mostrar sus conversiones
+  const [ratesData, setRatesData] = useState([]); //almacena los datos de la base
+
+  //Trae la data de la API al renderizar la página y los aloja en 'ratesData'
+  useEffect(() => {
+    latestRates(base).then(data => setRatesData(data))
+  }, [base]);
+
+  //Al componente Rates se le pasa la data para mostrar y 'setBase' para definir la base
+  //en un input
   return (
-    <Title />
+    <>
+      <Title /> 
+      <Rates ratesData={ratesData} setBase={setBase}/>
+    </>
   );
 }
 
