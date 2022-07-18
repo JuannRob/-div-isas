bce1bc40d80a37e75cb3bcb3a5f94ebc2aa25937
Kriss <kriss.minano@bbva.com>
2022-07-18 18:10:29 -0500

    bootstrap

--- src/components/Conversor.jsx
+++ src/components/Conversor.jsx
@@ -1,56 +1,131 @@
-import React, { useState } from 'react';
-import { convertCurrency } from '../services/API.js'
-import '../css/Conversor.css'
+import React, { useState } from "react";
+import icono from "../img/moneda.png";
+import "../css/Conversor.css";
+import { convertCurrency } from "../services/API.js";
 
 const Conversor = () => {
-	const [from, setFrom] = useState("");
-	const [to, setTo] = useState("");
-	const [monto, setMonto] = useState("");
-	const [result, setResult] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [monto, setMonto] = useState("");
+  const [result, setResult] = useState("");
 
-	const cambiaYA = (e) => {
-		e.preventDefault();
-		let myPromise = new Promise(function(myResolve, myReject) {
-			
-			myResolve(
-				convertCurrency(from, to, monto)
-			);
-  			myReject();
-		});
-		myPromise.then(
-			function(value) { setResult(value); },
-			function(err) { console.error(err); }
-		)
-	}
+  const cambiaYA = (e) => {
+    e.preventDefault();
+    let myPromise = new Promise(function (myResolve, myReject) {
+      myResolve(convertCurrency(from, to, monto));
+      myReject();
+    });
+    myPromise.then(
+      function (value) {
+        setResult(value);
+      },
+      function (err) {
+        console.error(err);
+      }
+    );
+  };
 
-	return(
-		<div className="Conversor">
-			<form onSubmit={cambiaYA} className="formConvertir">
-				<input 
-					type="text" 
-					value={from}
-					onChange={(e) => setFrom(e.target.value)}
-					placeholder="Ingrese Moneda 1"
-					className="inputBlanco"
-				></input>		
-				<input 
-					type="text" 
-					value={to}
-					onChange={(e) => setTo(e.target.value)}
-					placeholder="Ingrese Moneda 2"
-					className="inputBlanco"
-				></input>
-				<input 
-					type="number"
-					value={monto}
-					onChange={(e) => setMonto(e.target.value)}
-					placeholder="Ingrese Monto"
-				></input>
-				<input type="submit" value="CambiaYA"></input>
-			</form>
-			<div className="Resultado">{result.result}</div>
-		</div>
-	);
-}
+  return (
+    <div className="container text-center">
+      <img src={icono} className="icono"></img>
+      <div className="card text-center bg-light">
+        <div className="card-header">
+          <ul className="nav nav-tabs card-header-tabs">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="true" href="#">
+                Convertir
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="card-body">
+          <form onSubmit={cambiaYA} className="formConvertir">
+            <div className="row">
+              <div className="col">
+                <div className="mb-3">
+                  <label htmlFor="monedaDe" className="form-label">
+                    De:
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="monedaDe"
+                    value={from}
+                    onChange={(e) => setFrom(e.target.value)}
+                    placeholder="Ingrese Moneda 1"
+                  ></input>
+                  <div id="monedaDe" className="form-text">
+                    Moneda de origen ejemplo USD,PEN ARG
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="mb-3">
+                  <label htmlFor="monedaA" className="form-label">
+                    A:
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="monedaA"
+                    value={to}
+                    onChange={(e) => setTo(e.target.value)}
+                    placeholder="Ingrese Moneda 2"
+                  ></input>
+                  <div id="monedaA" className="form-text">
+                    Moneda de destino ejemplo USD,PEN ARG
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="mb-3">
+                  <label htmlFor="importe" className="form-label ">
+                    Importe:
+                  </label>
+
+                  <div className="row g-2 align-items-center">
+                    <div className="col-auto">
+                      <input
+                        type="number"
+                        className="form-control"
+                        id="importe"
+                        value={monto}
+                        onChange={(e) => setMonto(e.target.value)}
+                        placeholder="Ingrese importe"
+                      ></input>
+                    </div>
+                    <div className="col-auto">{from}</div>
+                  </div>
+
+                  <div id="importe" className="form-text">
+                    Monto a convertir por ejemplo 100,500, 1000
+                  </div>
+                </div>
+              </div>
+            </div>
+            <input
+              type="submit"
+              className="btn btn-primary convertir"
+              value="Convertir"
+            ></input>
+          </form>
+          <div className="Resultado">
+            {result.result > 0 && (
+              <>
+                <p>
+                  {monto} {from} = {result.result} {to}
+                </p>
+                <p>
+                  1 {from} = {result.info.rate} {to}
+                </p>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Conversor;
--- src/components/Rates.jsx
+++ src/components/Rates.jsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react'
-import '../css/Rates.css'
+import React, { useState } from "react";
+import "../css/Rates.css";
 
 const Rates = ({ ratesData, base, setBase }) => {
+  const [baseInput, setBaseInput] = useState(""); //acá se almacen los cambios que se van haciendo en el input
+  const [filters, setFilters] = useState([
+    "USD",
+    "ARS",
+    "EUR",
+    "GBP",
+    "JPY",
+    "BTC",
+  ]);
 
-  const [baseInput, setBaseInput] = useState(''); //acá se almacen los cambios que se van haciendo en el input
-  const [filters, setFilters] = useState(['USD', 'ARS', 'EUR', 'GBP', 'JPY', 'BTC']);
-  
   //Cambia la base de baseInput cada vez que se cambia en el input
   const handleChange = (e) => {
-    setBaseInput(e.target.value)
+    setBaseInput(e.target.value);
   };
 
   //Cuando se apreta Mostrar cambia la base de la app segun lo que esté asignado en 'baseInput'
   const changeBase = (e) => {
     e.preventDefault();
     setBase(baseInput);
-    filterRates()
-  }
+    filterRates();
+  };
 
   //Filtra las conversiones más relevantes
   const filterRates = () => {
@@ -25,9 +31,9 @@ const Rates = ({ ratesData, base, setBase }) => {
     // }
     let filteredRates = {};
     filters.forEach((curr) => {
-      filteredRates[curr] = ratesData.rates[curr]
-    })
-    return filteredRates
+      filteredRates[curr] = ratesData.rates[curr];
+    });
+    return filteredRates;
   };
 
   console.log(ratesData.rates);
@@ -35,28 +41,62 @@ const Rates = ({ ratesData, base, setBase }) => {
   // Tiene un formulario para cambiar la divisa base, un párrafo que indica la base actual y una lista con todas sus conversiones.
   return (
     <div className="form">
-      <form onSubmit={changeBase}>
-        <label>
-          <p>
-            Moneda:
-          </p>
-          <input type="text" value={baseInput} onChange={handleChange} placeholder='AUD, CAD, CHF' />
-        </label>
-        <input type="submit" value="Mostrar" />
-      </form>
-      <p id='base-p'>Conversiones del <b>{ratesData.base}</b>:</p>
-      <ul>
+      <div className="table-conversion">
+        <form onSubmit={changeBase} className="row g-3">
+          <h4>Buscar conversiones de tu moneda</h4>
+          <div className="col">
+            <div className="row">
+              <label htmlFor="moneda" className="col col-form-label">
+                Moneda:
+              </label>
+              <div className="col">
+                <input
+                  type="text"
+                  id="moneda"
+                  className="form-control"
+                  value={baseInput}
+                  onChange={handleChange}
+                  placeholder="AUD, CAD, CHF"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <input type="submit" className="btn btn-primary" value="Mostrar" />
+          </div>
+        </form>
+        <br></br>
+        <p id="base-p">
+          Convertir 1 <b>{ratesData.base}</b> a:
+        </p>
         {ratesData.rates ? (
-          Object.entries(filterRates()).map(([key, value], index) => (
-            <li key={index}><b>{key}</b>: {value.toString()}</li>
-          ))
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Moneda</th>
+                <th scope="col">Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {ratesData.rates &&
+                Object.entries(filterRates()).map(([key, value], index) => (
+                  <tr key={index}>
+                    <td>
+                      {baseInput} -><b>{key}</b>
+                    </td>
+                    <td>
+                      {value.toString()} {key}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         ) : (
-          <li>Actualizando información...</li>
+          <p>Actualizando información...</p>
         )}
-      </ul>
+      </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default Rates
+export default Rates;
